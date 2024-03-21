5418330cc9fcfaacdbde1d836b3fa86959346656
Alexander Tobias Ysbæk-Nielsen <atyn@outlook.dk>
2024-03-21 16:51:26 +0100

    Update run_physio.m
    separate peaks in to conditions in block-design paradigms

--- run_physio.m
+++ run_physio.m
@@ -36,6 +36,29 @@ for i = 1:length(subjects)
 
             % use physio_preproc.m
             physio_preproc(physiolog_path, output_mat_path, output_png_path);
+
+            % load preprocessed .mat
+            load(output_mat_path, 'preprocessedResp');
+
+            % define odor and air blocks
+            odor_intervals = [17, 32; 49, 64; 81, 96; 113, 128; 145, 160; 177, 192];
+            air_intervals = [1, 16; 33, 48; 65, 80; 97, 112; 129, 144; 161, 176; 193, 200];
+
+            % separate preprocessed peaks in to odor and air peaks
+            preprocessedResp.odoronsets = [];
+            preprocessedResp.aironsets = [];
+
+            for k = 1:size(odor_intervals, 1)
+                preprocessedResp.odoronsets = [preprocessedResp.odoronsets; preprocessedResp.peaks(preprocessedResp.peaks >= odor_intervals(k, 1) & preprocessedResp.peaks <= odor_intervals(k, 2))];
+            end
+
+            for k = 1:size(air_intervals, 1)
+                preprocessedResp.aironsets = [preprocessedResp.aironsets; preprocessedResp.peaks(preprocessedResp.peaks >= air_intervals(k, 1) & preprocessedResp.peaks <= air_intervals(k, 2))];
+            end
+
+            % save separated variables
+            save(output_mat_path, 'preprocessedResp', '-append');
+        
         end
     end
 end
